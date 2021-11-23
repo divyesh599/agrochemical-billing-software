40174d7e01fa00e6103eaf91e36e121b15995bb3
Divyesh Ranpariya <divyeshranpariya599@gmail.com>
2021-11-23 18:21:01 +0530

    commit

--- mainfile23Nov.py
+++ mainfile23Nov.py
@@ -17,7 +17,7 @@ curr.execute("CREATE DATABASE IF NOT EXISTS Database23Nov")
 curr.execute("CREATE TABLE IF NOT EXISTS Database23Nov.customerData\
     (MobileNo BIGINT,\
     FName VARCHAR(20),\
-    MName VARCHAR(20),\
+    MName VARCHAR(40),\
     LName VARCHAR(20),\
     City VARCHAR(20),\
     Balance INT,\
@@ -33,6 +33,26 @@ curr.execute("CREATE TABLE IF NOT EXISTS Database23Nov.productData\
     SellPrice INT,\
     BuyPrice INT,\
     PRIMARY KEY (PID))")
-
+curr.execute("CREATE TABLE IF NOT EXISTS Database23Nov.AllBills\
+    (BillDate VARCHAR(20),\
+    BillNo BIGINT,\
+    MobileNo BIGINT,\
+    Name VARCHAR(100),\
+    City VARCHAR(20),\
+    CashDebit VARCHAR(10),\
+    BillAmount INT,\
+    PRIMARY KEY (BillNo))")
+curr.execute("CREATE TABLE IF NOT EXISTS Database23Nov.SubBillDetails\
+    (BillDate VARCHAR(20),\
+    MobileNo BIGINT,\
+    BillNo BIGINT,\
+    PID INT,\
+    Description VARCHAR(100),\
+    Company VARCHAR(40),\
+    BatchNo VARCHAR(20),\
+    SellPrice INT,\
+    CashDebit VARCHAR(10),\
+    BillAmount INT,\
+    PRIMARY KEY (BillNo))")
 conn.commit()
 conn.close()
