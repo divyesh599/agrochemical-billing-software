82fbad73483844bf00acadaf2e8691f18335d307
Divyesh Ranpariya <divyeshranpariya599@gmail.com>
2022-01-05 22:44:39 +0530

    commit

--- temp/xyz.py
+++ temp/xyz.py
@@ -1,31 +1,11 @@
 for _ in range(int(input())):
-    n = int(input())
-    arr=[]
-    x=[]
+    n=int(input())
+    arr=list(map(int, input().split(" ")))
 
-    for i in range(n):
-        l, r, c=map(int, input().split(" "))
-        if i==0:
-            arr.append([l, r, c])
-            x=[l, r, c]
-            print(x[2])
-        else:
-            if x[0]<=l<=r<=x[1]:
-                pass
-            elif l<=x[0]<=x[1]<=r:
-                pass
-            elif x[1] <= l and x[0] <=
-                y=[0, 0, 0]
-                # arr[j] & l,c,r
-                for j in range(i+1):
-                    if arr[j][1]<=l and arr[j][0]<=r:
-                        if 
-                    else:
-                        y[2]+=c
-                        y[0]=min(arr[j][0], l)
-                        y[1]=max(arr[j][1], r)
-            else:
-                x[2]+=c
-                x[0]=min(x[0], l)
-                x[1]=max(x[1], r)
-            print(x[2])
+    ans=arr[0]&arr[1]
+
+    for i in range(n-1):
+        for j in range(i+1, n):
+            ans=ans|(arr[i]&arr[j])
+    
+    print(ans)
